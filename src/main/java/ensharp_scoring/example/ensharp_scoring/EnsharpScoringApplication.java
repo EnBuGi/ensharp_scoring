6225af7b69015289323f591cfa14851dbe7c487c
vvineey <vvin506@naver.com>
2026-02-13 21:37:22 +0900

    init: initial empty project structure

---
+++ src/main/java/ensharp_scoring/example/ensharp_scoring/EnsharpScoringApplication.java
@@ -0,0 +1,13 @@
+package ensharp_scoring.example.ensharp_scoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsharpScoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsharpScoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ensharp_scoring/example/ensharp_scoring/EnsharpScoringApplicationTests.java
@@ -0,0 +1,13 @@
+package ensharp_scoring.example.ensharp_scoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsharpScoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
